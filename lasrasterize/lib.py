2e740b7c3b908f5a81f0ca7a241d7086aef33b74
Brandon Martin-Anderson <badhill@gmail.com>
2024-01-05 00:35:56 +0000

    Rewrote lib.py. It had a lot redundant code.

--- lasrasterize/lib.py
+++ lasrasterize/lib.py
@@ -6,88 +6,52 @@ from collections import namedtuple
 from scipy import ndimage as nd
 from collections import defaultdict
 
-SQRT_TWO = 2**0.5
 
 BBox = namedtuple("BBox", ["left", "bottom", "right", "top"])
+Layerdef = namedtuple("Laslayer_definition", ["pulse_return", "intensity"])
+Layerdef.__doc__ = """Defines a layer of a LAS file.
+
+Args:
+    pulse_return (int): The pulse return included in this layer. Positive values count from the first return, negative values count from the last return.
+    intensity (bool): Whether to output a raster of intensity values.
+"""
 
 
-class PointCloud:
-    """A lidar PointCloud, with points, intensity, return_nums, and num_returns per point."""
+def infer_raster_resolution(las_file: laspy.LasData, p: float = 0.95) -> float:
+    """
+    Infers the raster resolution of a given LAS file.
 
-    def __init__(self):
-        self.points = None
-        self.intensity = None
-        self.return_num = None
-        self.num_returns = None
+    This function uses the first return of the LAS file to infer the raster resolution. The resolution is chosen such that,
+    assuming a uniform distribution of points, the probability that a given raster cell will contain at least one point is
+    equal to the given probability p. For a point density of 1.0 and p=0.95, a pixel width will be about 1.73.
 
-    @classmethod
-    def _from_lasdata(cls, las: laspy.LasData) -> "PointCloud":
-        """Instantiates a PointCloud object from a LASData object."""
+    Args:
+        las_file (laspy.LasData): The LAS file for which to infer the raster resolution.
+        p (float, optional): The probability that a given raster cell will contain at least one point.
 
-        ret = cls()
+    Returns:
+        float: The inferred raster resolution of the LAS file.
+    """
 
-        ret.points = np.column_stack((las.x, las.y, las.z))
-        ret.intensity = np.array(las.intensity) / 255.0
-        ret.return_num = np.array(las.return_num)
+    if p <= 0 or p >= 1:
+        raise ValueError("p must be between 0 and 1")
 
-        # cast to signed into so subtraction can result in negative numbers
-        ret.num_returns = np.array(las.num_returns).astype(int)
+    # find number of points in first return
+    first_return_count = las_file.header.number_of_points_by_return[0]
 
-        return ret
+    # find area of the bounding box of the LAS file
+    left, bottom, _ = las_file.header.min
+    right, top, _ = las_file.header.max
+    area = (right - left) * (top - bottom)
 
-    @classmethod
-    def from_laspy(cls, laspy_file: str | laspy.LasData) -> "PointCloud":
-        """Instantiates a PointCloud object from a LAS file."""
+    # find density of points in first return
+    density = first_return_count / area
 
-        if isinstance(laspy_file, laspy.LasData):
-            return cls._from_lasdata(laspy_file)
+    # this function finds the length of the side of a square that has probability p of containing at last one point given a point density of rho
+    # it's derived from the poisson distribution
+    resolution = lambda p, rho: np.sqrt(np.log(1 - p) / -rho)
 
-        with laspy.open(laspy_file) as laspy_file:
-            las = laspy_file.read()
-
-            return cls._from_lasdata(las)
-
-    @property
-    def bbox(self) -> BBox:
-        """Returns a BBox object representing the bounding box of the point"""
-
-        left, bottom, _ = self.points.min(axis=0)
-        right, top, _ = self.points.max(axis=0)
-        return BBox(left, bottom, right, top)
-
-    def get_layer(self, layer: int) -> "PointCloud":
-        """Returns a new PointCloud consisting of only points from a given layer.
-
-        If 'layer' is positive, returns points with the corresponding return number.
-        If 'layer' is negative, returns points with a return number counting from the last return,
-        e.g. -1 filters to last-return points.
-
-        Args:
-            layer (int): The layer number. Positive values correspond to the return number,
-                         negative values count from the last return.
-
-        Returns:
-            PointCloud: A new PointCloud object consisting of only points from the specified layer.
-
-        Raises:
-            ValueError: If the layer is zero.
-        """
-
-        if layer == 0:
-            raise ValueError("Layer must be positive or negative, not zero.")
-
-        if layer < 0:
-            layer = self.num_returns + layer + 1
-
-        mask = self.return_num == layer
-
-        ret = PointCloud()
-        ret.points = self.points[mask]
-        ret.intensity = self.intensity[mask]
-        ret.return_num = self.return_num[mask]
-        ret.num_returns = self.num_returns[mask]
-
-        return ret
+    return resolution(p, density)
 
 
 def fillholes(mat, radius: int = 1) -> np.ndarray:
@@ -102,6 +66,9 @@ def fillholes(mat, radius: int = 1) -> np.ndarray:
     Returns:
         np.ndarray: The input matrix with nan values filled.
     """
+
+    if radius < 0:
+        raise ValueError("Radius cannot be negative.")
 
     if radius == 0:
         return mat
@@ -126,238 +93,144 @@ def fillholes(mat, radius: int = 1) -> np.ndarray:
     return ret
 
 
-def pointcloud_to_rasters(
-    point_cloud: PointCloud,
+def lasdata_to_rasters(
+    lasdata: laspy.LasData,
     bbox: BBox,
+    layer_defs: Iterable[Layerdef],
     xres: int | float,
     yres: int | float,
-    fill_radius: int,
-) -> tuple[dict[str, np.ma.array], tuple[int, int]]:
-    """Converts a PointCloud object into a pair of rasters.
+    fill_holes: bool = True,
+    fill_radius: int = 2,
+) -> np.ndarray:
+    """Converts a lasdata object to a raster.
 
     Args:
-        point_cloud (PointCloud): The point cloud to convert.
+        lasdata (laspy.LasData): LasData object to convert.
         bbox (BBox): The bounding box to use for the conversion, in map units.
         xres (int | float): The resolution in the x direction, in map units.
         yres (int | float): The resolution in the y direction, in map units.
-        fill_radius (int): The radius to use when filling holes, in pixels.
+        layer_defs (Iterable[Laslayer_definition]): An iterable of Laslayer_definition objects, each defining a layer to output.
+        fill_holes (bool, optional): Whether to fill holes in the raster. Defaults to True.
+        fill_radius (int, optional): The radius to use when filling holes, in pixels.
 
     Returns:
-        tuple: A tuple of (rasters, shape). 'rasters' is a dictionary of rasters, with keys 'elev' and 'intensity'. 'shape' is a tuple of (n_cols, n_rows).
+        np.ndarray: An float array containing the elevation or intensity raster, with shape (n_layers, m, n). Null values are filled with np.nan.
     """
+
+    # find bounding box of the data
+    bbox = (
+        lasdata.header.x_min,
+        lasdata.header.y_min,
+        lasdata.header.x_max,
+        lasdata.header.y_max,
+    )
+
+    if xres is None or yres is None:
+        xres = yres = infer_raster_resolution(lasdata)
 
     n_rows = int((bbox.top - bbox.bottom) / yres) + 1
     n_cols = int((bbox.right - bbox.left) / xres) + 1
 
-    # get grid position of each point
-    j = ((point_cloud.points[:, 0] - bbox.left) / xres).astype(int)
-    i = ((bbox.top - point_cloud.points[:, 1]) / yres).astype(int)
+    i = ((bbox.top - lasdata.y) / yres).astype(int)
+    j = ((lasdata.x - bbox.left) / xres).astype(int)
 
     # set up nan-filled raster of the appropriate size
-    elev = np.full((n_rows, n_cols), np.nan)
-    intensity = np.full((n_rows, n_cols), np.nan)
+    rasters = np.full((len(layer_defs), n_rows, n_cols), np.nan)
 
-    # fill in grid positions with elevation information
-    # a large number of grid positions will not correspond
-    # to any lidar points and, as a result, will have NaN values
-    elev[i, j] = point_cloud.points[:, 2]
-    intensity[i, j] = point_cloud.intensity
+    for k, layer_def in enumerate(layer_defs):
+        # get a mask to filter out points that don't belong in this layer
+        if layer_def.layer < 0:
+            abs_pulse_return = lasdata.num_returns + layer_def.pulse_return + 1
+        else:
+            abs_pulse_return = layer_def.pulse_return
+        mask = (lasdata.return_num == abs_pulse_return).astype(bool)
 
-    elev = fillholes(elev, fill_radius)
-    intensity = fillholes(intensity, fill_radius)
+        # get grid position of each point
+        i, j = lasdata.i[mask], lasdata.i[mask]
 
-    return {
-        "elev": np.ma.array(elev, mask=np.isnan(elev)),
-        "intensity": np.ma.array(intensity, mask=np.isnan(intensity)),
-    }, (n_cols, n_rows)
+        # set up nan-filled raster of the appropriate size
+        raster = np.full((n_rows, n_cols), np.nan)
+
+        # fill in grid positions with elevation information
+        # a large number of grid positions will not correspond
+        # to any lidar points and, as a result, will have NaN values
+        if layer_def.intensity:
+            raster[i, j] = lasdata.intensity[mask]
+        else:
+            raster[i, j] = lasdata.z[mask]
+
+        if fill_holes:
+            raster = fillholes(raster, fill_radius)
+
+        rasters[k] = raster
+
+    return rasters
 
 
-def lidar_to_rasters(
+def lasfile_to_geotiff(
     las_filename: str,
-    layers: Iterable[int],
-    xres: int | float,
-    yres: int | float,
-    fill_radius: int,
-) -> tuple[dict[str, np.ndarray], BBox, tuple[int, int]]:
-    """Converts a LAS file to rasters.
-
-    This function takes a LAS file and an iterable of layers, and converts each layer into a raster.
-    The rasters are returned along with the bounding box and the shape of the rasters.
+    geotiff_filename: str,
+    layer_defs: Iterable[Layerdef],
+    xres: int | float | None = None,
+    yres: int | float | None = None,
+    fill_radius: int = 2,
+    crs: str = None,
+) -> None:
+    """Converts a LAS file to a GeoTiff.
 
     Args:
-        las_file (laspy.file.File): The LAS file to convert.
-        layers (Iterable[int]): An iterable of numbers specifying which return values to convert into a layer.
-                                For example, (1, 2, -1) would result in layers for the first, second, and last LIDAR return.
-        xres (int | float): The resolution in the x direction.
-        yres (int | float): The resolution in the y direction.
-        fill_radius (int): The radius to use when filling holes.
+        las_filename (str): The path to the LAS file to convert.
+        geotiff_filename (str): The path to the GeoTiff to output.
+        layer_defs (Iterable[Laslayer_definition]): An iterable of Laslayer_definition objects, each defining a layer to output.
+        xres (int | float | None, optional): The resolution in the x direction, in map units. If None, the resolution will be inferred from the LAS file. Defaults to None.
+        yres (int | float | None, optional): The resolution in the y direction, in map units. If None, the resolution will be inferred from the LAS file. Defaults to None.
+        fill_radius (int, optional): The radius to use when filling holes, in pixels.
+        crs (str, optional): The CRS of the output GeoTiff. If None, the CRS will be inferred from the LAS file. Defaults to None.
 
-    Returns:
-        tuple: A tuple containing a dictionary mapping theme names to stacks of rasters, the bounding box, and a tuple containing the width and height of the rasters.
-               Each stack of rasters has shape (num_rasters, m, n), where num_rasters is the length of the 'layers' arg.
+    Raises:
+        ValueError: If xres or yres is negative.
     """
 
-    # get lidar points, intensity, return num, and number of returns for each
-    # pulse
-    point_cloud = PointCloud.from_laspy(las_filename)
+    if xres is not None and xres < 0:
+        raise ValueError("xres cannot be negative")
+    if yres is not None and yres < 0:
+        raise ValueError("yres cannot be negative")
+
+    lasdata: laspy.LasData = laspy.read(las_filename)
 
     # find bounding box of the data
-    bbox = point_cloud.bbox
+    bbox = (
+        lasdata.header.x_min,
+        lasdata.header.y_min,
+        lasdata.header.x_max,
+        lasdata.header.y_max,
+    )
 
-    # produce dict of {theme name -> list of rasters}, where the list has a raster
-    # for each layer in 'layers'.
-    raster_layers = defaultdict(list)
-    shape = None
-    for layer in layers:
-        layer_pc = point_cloud.get_layer(layer)
+    if xres is None or yres is None:
+        xres = yres = infer_raster_resolution(lasdata)
 
-        rasters, layer_shape = pointcloud_to_rasters(
-            layer_pc, bbox, xres, yres, fill_radius
-        )
+    rasters = lasdata_to_rasters(lasdata, bbox, layer_defs, xres, yres, fill_radius)
 
-        # make sure the shape of every layer is the same
-        shape = shape or layer_shape
-        assert shape == layer_shape
+    if crs is None:
+        crs = lasdata.header.parse_crs()
 
-        for name, matrix in rasters.items():
-            raster_layers[name].append(matrix)
-
-    for name, layers in raster_layers.items():
-        raster_layers[name] = np.stack(layers).astype(np.float32)
-
-    return (raster_layers, bbox, shape)
-
-
-def to_geotiff(
-    themes: dict[str, np.ndarray],
-    bbox: BBox,
-    shape: tuple[int, int],
-    crs: str,
-    fn_out: str | IO,
-) -> None:
-    """Converts a processed LAS file to a GeoTiff.
-
-    Args:
-        themes (dict): A dictionary where the keys are theme names and the values are numpy arrays representing the theme layers with shape [n_layers, height, width].
-        bbox (BoundingBox): A BoundingBox object that defines the spatial extent of the output GeoTiff.
-        shape (tuple): A tuple defining the shape (width, height) of the output GeoTiff.
-        crs (str): A string defining the coordinate reference system, e.g., "+init=epsg:2926".
-        fn_out (str or file-like object): The filename our a file-like object to which to write the GeoTiff.
-
-    Returns:
-        None. The function writes directly to the file specified by `fn_out`.
-
-    """
-
-    width, height = shape
+    n_layers, height, width = rasters.shape
     transform = rio.transform.from_bounds(
         bbox.left, bbox.bottom, bbox.right, bbox.top, width, height
     )
 
-    a_theme = list(themes.values())[0]
-
-    n_themes = len(themes)
-    n_layers = len(a_theme)
-
     with rio.open(
-        fn_out,
+        geotiff_filename,
         "w",
         driver="GTiff",
         height=height,
         width=width,
-        count=n_themes * n_layers,
-        dtype=a_theme.dtype,
+        count=n_layers,
+        dtype=np.float32,
         crs=crs,
         transform=transform,
         compress="lzw",
         nodata=np.nan,
     ) as new_dataset:
-        i = 1
-        for theme_name, layers in themes.items():
-            print(f"writing theme:'{theme_name}'")
-            for layer in layers:
-                new_dataset.write(layer, i)
-                i += 1
-
-
-def infer_raster_resolution(las_file: laspy.LasData) -> float:
-    """
-    Infers the raster resolution of a given LAS file.
-
-    This function calculates the raster resolution by finding the number of points in the first return,
-    calculating the area of the bounding box of the LAS file, and then finding the area per point.
-    The resolution is then calculated as the width of a square with the same area as the area per point.
-
-    Args:
-        las_file (laspy.LasData): The LAS file for which to infer the raster resolution.
-
-    Returns:
-        float: The inferred raster resolution of the LAS file.
-    """
-
-    # find number of points in first return
-    first_return_count = las_file.header.number_of_points_by_return[0]
-
-    # find area of the bounding box of the LAS file
-    left, bottom, _ = las_file.header.min
-    right, top, _ = las_file.header.max
-    area = (right - left) * (top - bottom)
-
-    # find area per point
-    specific_area = area / first_return_count
-
-    res = round(specific_area**0.5 * SQRT_TWO, 2)
-
-    return res
-
-
-def las_to_raster(
-    file_in: str,
-    file_out: str,
-    crs: str,
-    return_num: list[int],
-    theme: list[str],
-    xres: float = None,
-    yres: float = None,
-    fill_radius: int = 2,
-) -> None:
-    """Convert LAS file to GeoTIFF raster.
-
-    Args:
-        file_in (str): Input LAS filename.
-        file_out (str): Output GeoTIFF filename.
-        crs (str): Coordinate reference system in whatever format
-            is accepted by the rasterio file constuctor. E.g. 'epsg:2926'.
-        return_num (list[int]): Return number(s) to rasterize, each in their own layer.
-            Negative numbers indicate position relative to last return; e.g. -1 is the last return.
-        theme (list[str]): Theme(s) to inclide. Choices are 'elev' and 'intensity'.
-        xres (float, optional): Width of one pixel in output GeoTIFF, in the horizontal
-            units of the CRS. If omitted, the LAS file will be used to make a reasonable guess.
-        yres (float, optional): Height of one pizel in output GeoTIFF, in the horizontal
-            units of the CRS. If omitted, the LAS file will be used to make a reasonable guess.
-        fill_radius (int, optional): Fill raster holes with average values within FILL_RADIUS pixels.
-    """
-
-    with laspy.open(file_in) as f:
-        las_data = f.read()
-
-        default_res = infer_raster_resolution(las_data)
-
-        xres = xres or default_res
-        yres = yres or default_res
-
-        themes, bbox, shape = lidar_to_rasters(
-            las_data,
-            layers=return_num,
-            xres=xres,
-            yres=yres,
-            fill_radius=fill_radius,
-        )
-
-        themes = {
-            theme_name: layers
-            for theme_name, layers in themes.items()
-            if theme_name in theme
-        }
-
-        to_geotiff(themes, bbox, shape, crs, file_out)
+        for i, layer in enumerate(rasters):
+            new_dataset.write(layer, i + 1)
